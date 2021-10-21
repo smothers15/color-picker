fb47b09bfadbc25eb37362221834499fbb89b746
smothers15 <dylo@fresnou.org>
2021-10-21 08:41:02 -0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index',{
+    color: "#FFFFFF",
+    textColor: "#FFFFFF"
+  });
+});
+
+router.post('/', function (req, res) {
+  res.render('index', {
+    color: req.body.colorPicker,
+    textColor: req.body.textColorPicker
+  })
+});
+
+router.post('/random', function (req, res) {
+  res.render('index', {
+    color: generateRandomHexCode(),
+    textColor: generateRandomHexCode()
+  })
+});
+
+function generateRandomHexCode(){
+  let hexCode = "#"
+  while (hexCode.length < 7) {
+    hexCode += (Math.round(Math.random()*15)).toString(16)
+  }
+  return hexCode
+}
+
+module.exports = router;
